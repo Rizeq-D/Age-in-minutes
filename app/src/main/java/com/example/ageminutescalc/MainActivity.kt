4092a97ca4a8c2ccb9115296ac34e674fe939ce4
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-08-07 14:13:35 +0200

    added 2 layers to the app

--- app/src/main/java/com/example/ageminutescalc/MainActivity.kt
+++ app/src/main/java/com/example/ageminutescalc/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.ageminutescalc
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,8 +19,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.ageminutescalc.ui.theme.AgeMinutesCalcTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
+
         setContentView(R.layout.activity_main)
     }
 }
