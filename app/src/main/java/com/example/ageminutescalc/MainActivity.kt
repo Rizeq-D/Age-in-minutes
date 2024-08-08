cb5f277a2b16dde04d164407e1b23180de4799e1
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-08-08 13:48:59 +0200

    added the actual date to the app

--- app/src/main/java/com/example/ageminutescalc/MainActivity.kt
+++ app/src/main/java/com/example/ageminutescalc/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.ageminutescalc
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,8 +20,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ageminutescalc.ui.theme.AgeMinutesCalcTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
+
+    private var theSelectedDate : TextView? = null
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +39,58 @@ class MainActivity : ComponentActivity() {
                         or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_FULLSCREEN
                         or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                )
-
+        )
         setContentView(R.layout.activity_main)
+
+        val selectBtn : Button = findViewById(R.id.btn_click_to_select)
+        theSelectedDate = findViewById(R.id.selected_date)
+
+        selectBtn.setOnClickListener{
+            setButton1()
+        }
+    }
+
+    fun setButton1() {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            {view, year, month, dayOfMonth ->
+                Toast.makeText(this, "Date selected", Toast.LENGTH_LONG).show()
+
+                val dateSelecting = "$day/${month+1}/$year"
+                theSelectedDate?.text = dateSelecting
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(dateSelecting)
+
+            },
+            year,
+            month,
+            day
+        ).show()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
