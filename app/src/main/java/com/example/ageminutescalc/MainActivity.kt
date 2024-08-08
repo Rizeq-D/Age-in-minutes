d2fc428ba821466975470080ccf75cddd4f208b6
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-08-08 15:16:33 +0200

    changed the colors of the background,foreground, and the texts

--- app/src/main/java/com/example/ageminutescalc/MainActivity.kt
+++ app/src/main/java/com/example/ageminutescalc/MainActivity.kt
@@ -8,18 +8,6 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ageminutescalc.ui.theme.AgeMinutesCalcTheme
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -27,6 +15,7 @@ import java.util.Locale
 class MainActivity : ComponentActivity() {
 
     private var theSelectedDate : TextView? = null
+    private var theAgeInMinutes : TextView? = null
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,37 +30,46 @@ class MainActivity : ComponentActivity() {
                         or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         )
         setContentView(R.layout.activity_main)
-
         val selectBtn : Button = findViewById(R.id.btn_click_to_select)
-        theSelectedDate = findViewById(R.id.selected_date)
+
+        theSelectedDate = findViewById(R.id.the_date)
+        theAgeInMinutes = findViewById(R.id.the_minutes)
 
         selectBtn.setOnClickListener{
             setButton1()
         }
     }
 
-    fun setButton1() {
+    private fun setButton1() {
 
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(this,
-            {view, year, month, dayOfMonth ->
+        val dpd = DatePickerDialog(this,
+            {view, selectedYear, selectedMonth, selectedDay ->
                 Toast.makeText(this, "Date selected", Toast.LENGTH_LONG).show()
 
-                val dateSelecting = "$day/${month+1}/$year"
+                val dateSelecting = "$selectedDay/${selectedMonth+1}/$selectedYear"
                 theSelectedDate?.text = dateSelecting
 
                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
                 val theDate = sdf.parse(dateSelecting)
+                theDate?.let {
+                    val selectedDateInMinutes = theDate.time/ 60000
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    currentDate?.let {
+                        val currentDateInMinutes = currentDate.time/ 60000
+                        val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
 
-            },
-            year,
-            month,
-            day
-        ).show()
+                        theAgeInMinutes?.text = differenceInMinutes.toString()
+                    }
+                }
+            }, year, month, day
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
     }
 }
 
